f3c6274fc8b58ca137a827eed4d78b915abb3cf2
Rafael Vieira Bravo <rafaelvb@al.insper.edu.br>
2025-09-11 09:57:26 -0300

    Parte do pacote

--- aps3/client/pacote.py
+++ aps3/client/pacote.py
@@ -14,10 +14,17 @@ class Package:
         self.eop = bytearray((69, 69, 69))
         self.file_size = 0
         self.content = bytes(0)
+        self.header = self.cria_header()
         pass
     
     def cria_header(self):
         h2 = self.file_size # tamanho da mensagem
         h3 = ceil(self.file_size / 100) # numero de pacotes
+        h4 = 1
+        h5 = 0
+        h6 = None
+        h7 = None
+        
+        _bytes = [1, h2, h3, h4, h5, h6, h7]
         
         pass
